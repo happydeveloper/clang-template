78ef7246bbf17b2a76cf3c97ace76988efaf0774
duruttmik <duru@talktomeinkorean.com>
2025-01-30 19:52:48 +0900

    플랫폼 구분 메시지 추가, 한국어 추가 Update platform-specific messages to include Korean translations

--- platform.cpp
+++ platform.cpp
@@ -3,18 +3,18 @@
 
 #if defined(_WIN32) || defined(_WIN64)
 void platform_specific_function() {
-    std::cout << "Running on Windows" << std::endl;
+    std::cout << "원도우에서 구동 중, Running on Windows" << std::endl;
 }
 #elif defined(__APPLE__) || defined(__MACH__)
 void platform_specific_function() {
-    std::cout << "Running on macOS" << std::endl;
+    std::cout << "맥에서 구동 중, Running on macOS" << std::endl;
 }
 #elif defined(__linux__)
 void platform_specific_function() {
-    std::cout << "Running on Linux" << std::endl;
+    std::cout << "리눅스에서 구동 중, Running on Linux" << std::endl;
 }
 #else
 void platform_specific_function() {
-    std::cout << "Unknown platform" << std::endl;
+    std::cout << "알수 없음, Unknown platform" << std::endl;
 }
 #endif
